8a4a916a4b51c4352026330d4d2c50d02859f145
Shajan Jacob <shajanjp@gmail.com>
2017-08-29 23:28:55 +0530

    adds node app folder and files

---
+++ config/modules.js
@@ -0,0 +1,14 @@
+var activeModules = [
+{
+	"name": "home",
+	"title": "BlackFire",
+	"url": "/"
+},
+{
+	"name": "persons",
+	"title": "Persons",
+	"url": "/persons"
+}
+];
+
+exports.activeModules = activeModules;
