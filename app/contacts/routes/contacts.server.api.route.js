3934db95cf1e3218b2b7937171fb1e0ecc325e1a
Shajan Jacob <shajanjp@gmail.com>
2017-09-07 20:10:11 +0530

    adds fetch contact API

--- app/contacts/routes/contacts.server.api.route.js
+++ app/contacts/routes/contacts.server.api.route.js
@@ -2,8 +2,10 @@ var contactsController = require('../controllers/contacts.server.controller.js')
 var domainRoot = '/api';
 
 module.exports = function(app){
+	app.route(domainRoot + '/contacts/list')
+	.get(contactsController.fetchContactAPI); // home.ejs
+	
 	app.route(domainRoot + '/contacts')
-	.get(contactsController.home) // home.ejs
 	.post(contactsController.createContactAPI);
 
 	app.param('contact_id', contactsController.contactById);
